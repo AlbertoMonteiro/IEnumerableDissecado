b196c4af4b2e2dbf9654b2e73312b41a06240365
Alberto Monteiro <alberto.monteiro@live.com>
2014-10-15 20:16:15 -0300

    Numeros de 1 a 10

--- IEnumerableDissecado/Program.cs
+++ IEnumerableDissecado/Program.cs
@@ -10,6 +10,24 @@ namespace IEnumerableDissecado
     {
         static void Main(string[] args)
         {
+            foreach (var i in NumerosDe1a10())
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static IEnumerable<int> NumerosDe1a10()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 3;
+            yield return 4;
+            yield return 5;
+            yield return 6;
+            yield return 7;
+            yield return 8;
+            yield return 9;
+            yield return 10;
         }
     }
 }
