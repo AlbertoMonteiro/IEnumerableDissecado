6b9dba51870002a67b59c30ea769938348426db8
Alberto Monteiro <alberto.monteiro@live.com>
2014-10-15 20:46:32 -0300

    Estado -1 tratando excessão

---
+++ IEnumerableDissecado/MaquinaEstado.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerableDissecado
+{
+    public class MaquinaEstado : IEnumerator<int>, IEnumerator
+    {
+        private readonly int i;
+        private int estado;
+
+        public MaquinaEstado(int i)
+        {
+            this.i = i;
+            estado = 0;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            switch (estado)
+            {
+                case 0: Current = 1; estado++; return true;
+                case 1: Current = 2; estado++; return true;
+                case 2: Current = 3 / i; estado++; return true;
+                case 3: Current = 4; estado++; return true;
+                case 4: Current = 5; estado++; return true;
+                case 5: Current = 6; estado++; return true;
+                case 6: Current = 7; estado++; return true;
+                case 7: Current = 8; estado++; return true;
+                case 8: Current = 9; estado++; return true;
+                default: return false;
+            }
+        }
+
+        public void Reset()
+        {
+            throw new NotSupportedException("Crie uma nova instância.");
+        }
+
+        public int Current { get; private set; }
+
+        object IEnumerator.Current
+        {
+            get { return Current; }
+        }
+    }
+}
--- IEnumerableDissecado/Program.cs
+++ IEnumerableDissecado/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,19 +11,48 @@ namespace IEnumerableDissecado
     {
         static void Main(string[] args)
         {
-            var numerosDe1A10 = NumerosDe1a10().Onde(x => x < 10);
+            var maquinaEstado = NumerosDe1a10(0);
+            //var maquinaEstado = new MaquinaEstado(0);
+
+            try
+            {
+                while (maquinaEstado.MoveNext())
+                {
+                    Console.WriteLine(maquinaEstado.Current);
+                }
+            }
+            catch (Exception)
+            {
+                
+            }
+            
+            while (maquinaEstado.MoveNext())
+            {
+                Console.WriteLine(maquinaEstado.Current);
+            }
+
+            /*var numerosDe1A10 = NumerosDe1a10().Onde(x => x < 10);
 
             foreach (var i in numerosDe1A10)
             {
                 Console.WriteLine(i);
-            }
+            }*/
         }
 
-        public static IEnumerable<int> NumerosDe1a10()
+        public static IEnumerator<int> NumerosDe1a10(int x)
         {
-            int i = 0;
+           /* int i = 0;
             while (true)
-                yield return i++;
+                yield return i++;*/
+            yield return 1;
+            yield return 2;
+            yield return 3/x;
+            yield return 4;
+            yield return 5;
+            yield return 6;
+            yield return 7;
+            yield return 8;
+            yield return 9;
         }
     }
 
